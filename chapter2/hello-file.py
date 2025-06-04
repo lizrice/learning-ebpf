428979026357b9ce252ca3524c45f498946bac6f
Liz Rice <liz@lizrice.com>
2025-06-04 17:16:34 +0000

    even simpler open file example

--- chapter2/hello-file.py
+++ chapter2/hello-file.py
@@ -8,7 +8,7 @@ TRACEPOINT_PROBE(syscalls, sys_enter_openat)
 
   bpf_get_current_comm(command, sizeof(command));
 
-  bpf_trace_printk("File %d - %s", args->dfd, args->filename);
+  bpf_trace_printk("File %s", args->filename);
   bpf_trace_printk("     opened by:%s", command);
 
   return 0;
